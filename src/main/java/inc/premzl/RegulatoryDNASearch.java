4c404ac2d383f1c425e29b469e877b9ce01aa172
Jan Premzl <jan.premzl16@gmail.com>
2022-04-20 21:18:29 +0200

    Updated tests

--- src/main/java/inc/premzl/RegulatoryDNASearch.java
+++ src/main/java/inc/premzl/RegulatoryDNASearch.java
@@ -26,19 +26,27 @@ public class RegulatoryDNASearch {
         } else if (Objects.equals(args[0], "test")) {
             BranchAndBoundSearch bnbSearch = new BranchAndBoundSearch(n, t, l, filename);
             GreedySearch greedySearch = new GreedySearch(n, t, l, filename);
+            Consensus consensus = new Consensus();
+
             long avgTime, start, end;
-            int iterations = 1000;
+            int iterations = 100;
 
             for (int opt = 0; opt < 2; opt++) {
                 avgTime = 0;
                 for (int i = 0; i < iterations; i++) {
                     start = System.nanoTime();
-                    if (opt == 0) greedySearch.greedySearch();
-                    else bnbSearch.branchAndBoundSearch();
+                    if (opt == 0) consensus = greedySearch.greedySearch();
+                    else consensus = bnbSearch.branchAndBoundSearch();
                     end = System.nanoTime();
                     avgTime += (end - start);
                 }
                 System.out.println((double) TimeUnit.MILLISECONDS.convert(avgTime, TimeUnit.NANOSECONDS) / iterations);
+                System.out.printf("N: %d\tT: %d\tL: %d\tALGORITHM: %s\tCONSENSUS: %s\t%s: %d\n",
+                        n, t, l,
+                        opt == 0 ? "GREEDY" : "BRANCH AND BOUND",
+                        consensus.getConsensus(),
+                        opt == 0 ? "SCORE" : "HAMMING",
+                        opt == 0 ? consensus.getScore() : consensus.getHamming());
             }
         } else throw new Exception("Invalid algorithm argument");
     }
