bc53e856b56a82014bc0e4b18994447a44486451
Jan Premzl <jan.premzl16@gmail.com>
2022-04-20 19:30:37 +0200

    Moved statements

--- src/main/java/inc/premzl/RegulatoryDNASearch.java
+++ src/main/java/inc/premzl/RegulatoryDNASearch.java
@@ -17,10 +17,10 @@ public class RegulatoryDNASearch {
         if (args.length != 5) throw new Exception("Invalid arguments size");
 
         List<String> sequences = splitSequence(readFile(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
-        List<List<String>> kmers = new ArrayList<>();
-        for (String sequence : sequences) kmers.add(getKmers(sequence, Integer.parseInt(args[4])));
 
         if (Objects.equals(args[0], "greedy")) {
+            List<List<String>> kmers = new ArrayList<>();
+            for (String sequence : sequences) kmers.add(getKmers(sequence, Integer.parseInt(args[4])));
             Consensus consensus = generateProfiles(kmers, Integer.parseInt(args[4]));
             System.out.println(consensus.getConsensus() + " " + consensus.getScore());
         } else if (Objects.equals(args[0], "bnb")) {
